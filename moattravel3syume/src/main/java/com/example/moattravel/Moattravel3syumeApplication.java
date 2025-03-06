5f0df7ffed12820efd7f58bba3f942f944069137
81806 <81806@192.168.10.101>
2025-03-06 14:29:44 +0900

    3周目

---
+++ moattravel3syume/src/main/java/com/example/moattravel/Moattravel3syumeApplication.java
@@ -0,0 +1,13 @@
+package com.example.moattravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Moattravel3syumeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Moattravel3syumeApplication.class, args);
+	}
+
+}
