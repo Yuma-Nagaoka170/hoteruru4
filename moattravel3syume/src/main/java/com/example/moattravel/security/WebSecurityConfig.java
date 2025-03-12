ff358f41219c6a63ab264cd6eca04a5e5a647413
81806 <81806@192.168.10.101>
2025-03-12 18:17:04 +0900

    18時コミット

--- moattravel3syume/src/main/java/com/example/moattravel/security/WebSecurityConfig.java
+++ moattravel3syume/src/main/java/com/example/moattravel/security/WebSecurityConfig.java
@@ -17,11 +17,8 @@ public class WebSecurityConfig {
 	@Bean
 	public SecurityFilterChain securityFileterChain(HttpSecurity http) throws Exception {
 		http
-		.csrf(csrf -> csrf
-	            .ignoringRequestMatchers("/batch/daily-logins") // ここで無効化
-	        )
 		.authorizeHttpRequests((requests) -> requests
-				.requestMatchers("/css/**", "/images/**", "/js/**", "/storage/**", "/", "/signup/**", "/batch/daily-logins") .permitAll()//全てのユーザーにアクセスを許可するURL
+				.requestMatchers("/css/**", "/images/**", "/js/**", "/storage/**", "/", "/signup/**", "/batch/daily-logins", "/houses") .permitAll()//全てのユーザーにアクセスを許可するURL
 				.requestMatchers("/admin/**").hasRole("ADMIN")//管理者にのみアクセスを許可する
 				.anyRequest().authenticated() //上記以外のURLはログインが必要（会員または管理者のどちらでもOK）
 				)
